cda03088a4ca7d2ff93a26ce516c8d816254aa7d
aimane yousr <aimaneyousr@gmail.com>
2024-05-19 18:16:22 +0100

    chore: Add ball struct and update rendering logic

--- src/main.c
+++ src/main.c
@@ -6,6 +6,13 @@ int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
+struct ball {
+  float x;
+  float y;
+  float width;
+  float height;
+} ball;
+
 int initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         fprintf(stderr, "Error initializing SDL.\n");
@@ -15,7 +22,7 @@ int initialize_window(void) {
     //                           int x, int y, int w,
     //                           int h, Uint32 flags);
     window = SDL_CreateWindow(
-        NULL,
+        "SDL2 \\o/",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH,
@@ -34,7 +41,7 @@ int initialize_window(void) {
         fprintf(stderr, "Error creating SDL Renderer\n");
         return FALSE;
     }
-    
+
     return TRUE;
 }
 
@@ -53,21 +60,36 @@ void process_input() {
     }
 }
 
-void update() {
+void setup() {
+  ball.x = 20;
+  ball.y = 20;
+  ball.width = 15;
+  ball.height = 15;
+}
 
+void update() {
+  ball.x += 1;
+  ball.y += 1;
 }
 
 void render() {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_RenderClear();
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
     // here is where we can start drawing our game objects
 
-    
-}
+    // first i'll draw a rectangle
+    SDL_Rect ball_rect = {
+      (int)ball.x,
+      (int)ball.y,
+      (int)ball.width,
+      (int)ball.height
+    };
 
-void setup() {
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(renderer, &ball_rect);
 
+    SDL_RenderPresent(renderer);
 }
 
 void destroy_window() {
@@ -78,8 +100,8 @@ void destroy_window() {
 
 
 int main() {
-    int game_is_running = initialize_window();
-    
+    game_is_running = initialize_window();
+
     setup();
 
     // the holy trinity of the game loop
@@ -89,8 +111,8 @@ int main() {
         update();
         render();
     }
-    
+
     destroy_window();
 
-    return 0;
+    return FALSE;
 }
